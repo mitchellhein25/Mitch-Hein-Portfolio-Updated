71d333ee3c48c88af13e3bf473de13fc528c8a45
mitchellhein25 <mitchelldalehein25@gmail.com>
2022-03-31 05:22:17 -0500

    Fixed Skills section header font size CSS.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c0a71f5951123311"; }
+/******/ 		__webpack_require__.h = function() { return "d084bd49751479bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.e5008625bbfdb0cf.hot-update.js
+++ .next/static/webpack/webpack.e5008625bbfdb0cf.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "128f7215d1ce5b42"; }
+/******/ 	__webpack_require__.h = function() { return "d084bd49751479bb"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
